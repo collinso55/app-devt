4770eea9d3b5b18d13bb253da88712e15ade06a6
tamanjong collins <collinstamanjong21@gmail.com>
2025-09-16 13:05:37 +0200

    first code for app devt

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import Counter from './Counter';
+import Wintro from './Wintro';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Counter);
